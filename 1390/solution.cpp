5003fb7520fef494ccc2822df084e5dc45add9b9
maksud.inamdar_cof <maksud.inamdar_cof@npci.org.in>
2026-01-20 22:28:13 +0530

    initial setup

---
+++ 1390/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ *    author:  maq5ud     
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+ 
+
+signed main() {
+    fast;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t; cin>>t;
+    while(t--) {
+        int n; cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        int k; cin >> k;
+
+        int ans = -1;
+        int low = 0, high = 0;
+        while (high < n) {
+            if ()
+        }
+    }
+    return 0;
+}
