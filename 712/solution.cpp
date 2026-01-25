14b17a2b5f2af30d112c5a9e382536c0b0e1a557
maqsudinamdar <maqsudinamdar@gmail.com>
2026-01-25 22:12:22 +0530

    ac

--- 712/solution.cpp
+++ 712/solution.cpp
@@ -6,24 +6,47 @@ using namespace std;
 #define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
  
 
-int sumFourDivisors(vector<int>& nums) {
-        int ans = 0;
-        for (int num: nums) {
-            int cnt = 0, sum = 0;
-            for (int i = 1; i <= sqrt(num); i++) {
-                if (num % i == 0) {
-                    sum += i; cnt++;
-                    if (i !=  num / i) {
-                        sum += (num / i); cnt++;
-                    }   
-                }
-            }
-            if (cnt == 4) {
-                ans += sum;
-            }
-        }
-        return ans;
+int f(int i, int j, string s1, string s2) {
+    if (i == 0 || j == 0) return 0;
+    
+    if (s1[i - 1] == s2[j - 1]) {
+        return (int)s1[i - 1] + f(i - 1, j - 1, s1, s2);
+    }
+    int x = f(i - 1, j, s1, s2);
+    int y = f(i, j - 1, s1, s2);
+
+    return max(x, y);
 }
+
+int topDown(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
+    if (i == 0 || j == 0) return 0;
+
+    if (dp[i][j] != -1) return dp[i][j];
+    
+    if (s1[i - 1] == s2[j - 1]) {
+        return (int)s1[i - 1] + topDown(i - 1, j - 1, s1, s2, dp);
+    }
+    int x = topDown(i - 1, j, s1, s2, dp);
+    int y = topDown(i, j - 1, s1, s2, dp);
+
+    return dp[i][j] = max(x, y);
+}
+
+int minimumDeleteSum(string s1, string s2) {
+    int m = s1.size(), n = s2.size();
+    
+    int total = 0;
+    for (int i = 0; i < m; i++) total +=  (int)s1[i];
+    for (int j = 0; j < n; j++) total +=  (int)s2[j];
+
+    // int x = f(m, n, s1, s2);
+    // return total - 2 * x;
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+    int y = topDown(m, n, s1, s2, dp);
+    return total - 2 * y;
+}
+
 
 signed main() {
     fast;
@@ -33,10 +56,8 @@ signed main() {
     #endif
     int t; cin>>t;
     while(t--) {
-        int n; cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-        cout << sumFourDivisors(a) << endl;
+        string s1, s2; cin >> s1 >> s2;
+        cout <<  minimumDeleteSum(s1, s2) << endl;
     }
     return 0;
 }
